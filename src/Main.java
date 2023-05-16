86dd8a076a0849d7be384ed97355ffb86ea24d94
Sett311 <2004sett@gmail.com>
2023-05-16 19:49:05 +0300

    reformat code

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,10 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        BmiService service = new BmiService();
+        int weight = 90; // вес в кг
+        int height = 180; // рост в см
+        double bmi = service.calculate(weight, height);
+        System.out.println(bmi);
     }
 }
