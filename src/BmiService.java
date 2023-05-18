134927e77fee8be7bd3b069ee2dd6a385bcff4e5
Sett311 <2004sett@gmail.com>
2023-05-18 20:21:34 +0300

    не залился с первого раза

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,2 +1,6 @@
-package PACKAGE_NAME;public class BmiService {
+public class BmiService {
+    public double calculate (int weight, int height) {
+        double h = (double) height / 100;
+        return weight / h / h;
+    }
 }
